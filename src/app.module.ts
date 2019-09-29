6a6cba9af6c95a457f8dd27382e4580f83768040
Riku Laajala <riku.laajala@gmail.com>
2019-09-29 12:35:33 +0300

    connection CRUD
    - Added sqlite as database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,24 @@
-import { Module } from '@nestjs/common';
-import { RouterModule, Routes } from 'nest-router';
+import {
+  Module,
+  MiddlewareConsumer,
+  RequestMethod,
+  NestModule,
+} from '@nestjs/common';
+
+// For typeorm
+import 'reflect-metadata';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabasesController } from './databases/databases.controller';
-import { TablesController } from './tables/tables.controller';
-import { DatabasesModule } from './databases/databases.module';
-import { TablesModule } from './tables/tables.module';
 
-const routes: Routes = [
-  {
-    path: '/databases',
-    module: DatabasesModule,
-    children: [
-      {
-        path: ':id/tables',
-        module: TablesModule,
-      },
-    ],
-  },
-];
+// Modules
+import { ConnectionModule } from './connection/connection.module';
 
 @Module({
   imports: [
-    RouterModule.forRoutes(routes), // setup the routes,
-    DatabasesModule,
-    TablesModule
+    TypeOrmModule.forRoot(),
+    ConnectionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
